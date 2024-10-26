098e29d733394dcb84573f4dc4cd772c7f548215
Pablopoke4 <pablomantovani05@gmail.com>
2024-10-25 22:10:14 -0300

    Tela de criação de conta - Aula do dia 25/10/2024

---
+++ Assets/Scripts/Resource.cs
@@ -0,0 +1,14 @@
+public static class Resource
+{
+    public readonly static string APIKey = "APIKEY";
+    public readonly static string BaseURL = "Localhost:3000/api/";
+    public readonly static string LoginPath = "users/Login";
+    public readonly static string CreateUserPath = "users/register";
+
+    public static class Screen
+    {
+        public readonly static string Login = "LoginScreen";
+        public readonly static string Principal = "PrincipalScreen";
+        public readonly static string CreateAccount = "CreateAccountScreen";
+    }
+}
